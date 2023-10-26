1c1f28a9ad735e25007d0d2f1d3efb5ac3f867af
Gwendoline Jacobs <gwendolinejacobs1@gmail.com>
2023-10-26 11:56:33 +0200

    code + opti + reset fonctionnel

--- script.js
+++ script.js
@@ -139,7 +139,7 @@ document.addEventListener('DOMContentLoaded', function() {
   ********************************/
 
   const pokeballElement = document.getElementById('pokeball');
-    const gameplayWrapper = document.getElementById('gameplay-wrapper');
+    const gameplayWrapper = document.getElementById('gameplay-interface');
     let animationFrameId = null;
     // let clickPower = 1; // Assurez-vous d'initialiser clickPower en fonction de la logique de votre jeu.
 
@@ -187,280 +187,96 @@ document.addEventListener('DOMContentLoaded', function() {
     pokeballElement.style.left = '0px';
 
     schedulePokeball(); 
-  
-  /********************************
-  
-            Attaque
-  
+
+   /********************************
+
+          Attaques
+
   ********************************/
-  
-  /********************************
-            Charge
-  ********************************/
-  
-  //DOM Content 
-    let chargeBuy = document.getElementById("charge-buy");
-    let chargePrice = document.getElementById("charge-price");
-    let chargeLevel = document.getElementById("charge-level");
-    let autoclickMultiple = document.getElementById("click-power-multiple");
-  
-    let chargePriceAmount = 50;
-    let chargeLevelNumber = 1;
-  
-  // Refresh level/Price
-  function refreshChargeLevel() {
-    chargeLevel.innerHTML = chargeLevelNumber;
-    chargePrice.innerHTML = chargePriceAmount;
-    autoclickMultiple.innerHTML = clickPower;
+    // DOM Content references
+    let autoclickMultipleAtt = document.getElementById("autoclick-multiple");
+    let autoclickMultipleAttAmount=1;
+  const items = [
+    { id: "charge", price: 50, level: 1, power: 1, initialPrice:50 },
+    { id: "etincelle", price: 200, level: 1, power: 2, initialPrice:200 },
+    { id: "fatalfoudre", price: 500, level: 1, power: 5, initialPrice:500 },
+    { id: "trempette", price: 100, level: 1, power: -10, initialPrice:100 }
+  ];
+
+  function refreshItemLevel(item) {
+    const element = document.getElementById(`${item.id}-level`);
+    const priceElement = document.getElementById(`${item.id}-price`);
+    element.innerHTML = item.level;
+    priceElement.innerHTML = item.price;
+    autoclickMultipleAtt.innerHTML= autoclickMultipleAttAmount;
   }
-  
-  // Booster click
-    chargeBuy.addEventListener("click", () => {
-      if (scoreCount >= chargePriceAmount) {
-        scoreCount -= chargePriceAmount;
-        chargeLevelNumber++;
-        chargePriceAmount = Math.floor(chargePriceAmount * 1.20);
-        clickPower+=1;
+
+  function buyItem(item) {
+    const itemElement = document.getElementById(`${item.id}-buy`);
+    itemElement.addEventListener("click", () => {
+      if (scoreCount >= item.price) {
+        scoreCount -= item.price;
+        item.level++;
+        item.price = Math.floor(item.price * 1.20);
+        clickPower += item.power;
+        autoclickMultipleAttAmount += item.power;
         refreshCookieScore();
-        refreshChargeLevel();
+        refreshItemLevel(item);       
         console.log(clickPower);
       } else {
         console.log('Not enough clicks!');
       }
     });
-  
-  /********************************
-            Etincelle
-  ********************************/
-  // DOM Content 
-  let etincelleBuy = document.getElementById("etincelle-buy");
-  let etincellePrice = document.getElementById("etincelle-price");
-  let etincelleLevel = document.getElementById("etincelle-level");
-  
-  let etincellePriceAmount = 200;
-  let etincelleLevelNumber = 1;
-  
-  // Refresh level/Price
-  function refreshEtincelleLevel() {
-    etincelleLevel.innerHTML = etincelleLevelNumber;
-    etincellePrice.innerHTML = etincellePriceAmount;
-    autoclickMultiple.innerHTML = clickPower;
   }
-  
-  // Booster click
-  etincelleBuy.addEventListener("click", () => {
-    if (scoreCount >= etincellePriceAmount) {
-      scoreCount -= etincellePriceAmount;
-      etincelleLevelNumber++;
-      etincellePriceAmount = Math.floor(etincellePriceAmount * 1.20);
-      clickPower += 2;
-      refreshCookieScore();
-      refreshEtincelleLevel();
-      console.log(clickPower);
-    } else {
-      console.log('Not enough clicks!');
-    }
+  items.forEach(item => {
+    buyItem(item);
   });
-  
-  /********************************
-            Fatal Foudre
-  ********************************/
-  // DOM Content 
-  let fatalfoudreBuy = document.getElementById("fatalfoudre-buy");
-  let fatalfoudrePrice = document.getElementById("fatalfoudre-price");
-  let fatalfoudreLevel = document.getElementById("fatalfoudre-level");
-  
-  let fatalfoudrePriceAmount = 500;
-  let fatalfoudreLevelNumber = 1;
-  
-  // Refresh level/Price
-  function refreshFatalfoudreLevel() {
-    fatalfoudreLevel.innerHTML = fatalfoudreLevelNumber;
-    fatalfoudrePrice.innerHTML = fatalfoudrePriceAmount;
-    autoclickMultiple.innerHTML = clickPower;
-  }
-  
-  // Booster click
-  fatalfoudreBuy.addEventListener("click", () => {
-    if (scoreCount >= fatalfoudrePriceAmount) {
-      scoreCount -= fatalfoudrePriceAmount;
-      fatalfoudreLevelNumber++;
-      fatalfoudrePriceAmount = Math.floor(fatalfoudrePriceAmount * 1.20);
-      clickPower += 5;
-      refreshCookieScore();
-      refreshFatalfoudreLevel();
-      console.log(clickPower);
-    } else {
-      console.log('Not enough clicks!');
-    }
-  });
-  
-  /********************************
-            Trempette
-  ********************************/
-  // DOM Content 
-  let trempetteBuy = document.getElementById("trempette-buy");
-  let trempettePrice = document.getElementById("trempette-price");
-  let trempetteLevel = document.getElementById("trempette-level");
-  
-  let trempettePriceAmount = 100;
-  let trempetteLevelNumber = 1;
-  
-  // Refresh level/Price
-  function refreshtrempetteLevel() {
-    trempetteLevel.innerHTML = trempetteLevelNumber;
-    trempettePrice.innerHTML = trempettePriceAmount;
-    autoclickMultiple.innerHTML = clickPower;
-  }
-  
-  // Booster click
-  trempetteBuy.addEventListener("click", () => {
-    if (scoreCount >= trempettePriceAmount) {
-      scoreCount -= trempettePriceAmount;
-      trempetteLevelNumber++;
-      trempettePriceAmount = Math.floor(trempettePriceAmount * 1.20);
-      clickPower += -10;
-      refreshCookieScore();
-      refreshChargetrempetteLevel();
-      console.log(clickPower);
-    } else {
-      console.log('Not enough clicks!');
-    }
-  });
-  
   /********************************
 
           Shop
 
   ********************************/
-    /********************************
-            Potion
-  ********************************/
-  //DOM Content 
-
-  let potionBuy = document.getElementById ("potion-buy");
-  let potionPrice = document.getElementById("potion-price");
-  let potionLevel = document.getElementById("potion-level");
-  let allMultiple2 = document.getElementById("autoclickmultiple2");
-  let potionPower = 1;
-  let potionPriceAmount = 500;
-  let potionLevelNumber = 0;
   let allMultiple = 0;
+  let allMultiple2 = document.getElementById("autoclickmultiple2");
 
-  // Fonction de loop
-  let potionInt;
-  function potionAutoStart() {
-    potionInt = window.setInterval(()=> {
-      scoreCount = scoreCount + potionPower;
-      refreshCookieScore();
-    }, 1000);
-  }
-
-  // Rafraîchissement des HTML Pika
-  function refreshPotion () {
-    potionLevel.innerHTML = potionLevelNumber;
-    potionPrice.innerHTML = potionPriceAmount;
-    allMultiple += potionPower * potionLevelNumber;
-    allMultiple2.innerHTML = allMultiple;
-  }
-  // Pika on click ajout
-  potionBuy.addEventListener ("click", ()=> {
-  //update du prix + exécution du loop 
-    if (scoreCount >= potionPriceAmount){
-      scoreCount -= potionPriceAmount;
-      refreshCookieScore();
-      potionLevelNumber +=1; 
-      potionPriceAmount = Math.floor(potionPriceAmount * 1.20);
-      refreshPotion();
-      potionAutoStart();
-    }
-  })
-
-    /********************************
-            Multi-Exp
-  ********************************/
-
-  //DOM Content 
-  let multiexpBuy = document.getElementById("multiexp-buy");
-  let multiexpPrice = document.getElementById("multiexp-price");
-  let multiexpLevel = document.getElementById("multiexp-level");
-  // let multiexpMultiple = document.getElementById("multiexp-multiple");
-
-  let multiexpPower = 5;
-  let multiexpPriceAmount = 2500;
-  let multiexpLevelNumber = 0;
-
-  //Rafraîchir HTML
-  function refreshMultiexp() {
-    multiexpLevel.innerHTML = multiexpLevelNumber;
-    multiexpPrice.innerHTML = multiexpPriceAmount;
-    allMultiple += multiexpPower * multiexpLevelNumber;
+  const itemsData = [
+    { id: "potion", price: 500, power: 1, initialPrice:500, level:0},
+    { id: "multiexp", price: 2500, power: 5, initialPrice :2500, level:0},
+    { id: "terrain", price: 10000, power: 25, initialPrice:10000, level:0}
+  ];
+  
+  function refreshItem(item) {
+    const itemLevel = document.getElementById(`${item.id}-level`);
+    const itemPrice = document.getElementById(`${item.id}-price`);
+    itemLevel.innerHTML = item.level;
+    itemPrice.innerHTML = item.price;
     allMultiple2.innerHTML = allMultiple;
   }
 
-  //Loop Multiexp
-  let multiexpInt;
-  function multiexpStart () {
-      multiexpInt = window.setInterval(() => {
-      scoreCount+= multiexpPower;
-      refreshCookieScore();
-    }, 1000);
-  }
-
-  // Event sur le click de multiexp
-  multiexpBuy.addEventListener("click", () => {
-    if (scoreCount >= multiexpPriceAmount) {
-      scoreCount-=multiexpPriceAmount;
-      refreshCookieScore ();
-      multiexpLevelNumber +=1;
-      multiexpPriceAmount = Math.floor(multiexpPriceAmount * 1.20);
-      multiexpStart();
-      refreshMultiexp();
-    }
+  let itemInt;
+  function buyItem2(item) {
+    const itemBuy = document.getElementById(`${item.id}-buy`);
+    itemBuy.addEventListener("click", () => {
+      if (scoreCount >= item.price) {
+        scoreCount -= item.price;
+        refreshCookieScore();
+        item.level++;
+        item.price = Math.floor(item.price * 1.20);
+        itemInt = window.setInterval(() => {
+          scoreCount += item.power;
+          refreshCookieScore();
+        }, 1000);
+        allMultiple += item.power * item.level;
+        refreshItem(item);
+      }
     });
-
-    /********************************
-            Terrain
-  ********************************/
-
-  //DOM Content 
-  let terrainBuy = document.getElementById("terrain-buy");
-  let terrainPrice = document.getElementById("terrain-price");
-  let terrainLevel = document.getElementById("terrain-level");
-  // let multiexpMultiple = document.getElementById("multiexp-multiple");
-
-  let terrainPower = 25;
-  let terrainPriceAmount = 10000;
-  let terrainLevelNumber = 0;
-
-  //Rafraîchir HTML
-  function refreshTerrain() {
-    terrainLevel.innerHTML = terrainLevelNumber;
-    terrainPrice.innerHTML = terrainPriceAmount;
-    allMultiple += terrainPower * terrainLevelNumber;
-    allMultiple2.innerHTML = allMultiple;
   }
-
-  //Loop Terrain
-  let terrainInt;
-  function terrainStart () {
-      terrainInt = window.setInterval(() => {
-      scoreCount+= terrainPower;
-      refreshCookieScore();
-    }, 1000);
-  }
-
-  // Event sur le click de Terrain
-  terrainBuy.addEventListener("click", () => {
-    if (scoreCount >= terrainPriceAmount) {
-      scoreCount-=terrainPriceAmount;
-      refreshCookieScore ();
-      terrainLevelNumber +=1;
-      terrainPriceAmount = Math.floor(terrainPriceAmount * 1.20);
-      terrainStart();
-      refreshTerrain();
-    }
+  
+  itemsData.forEach(item => {
+    item.level = 0;
+    buyItem2(item);
   });
+  
     /********************************
       
             Pierre évolutive
@@ -508,41 +324,24 @@ document.addEventListener('DOMContentLoaded', function() {
             Reset
 
   ********************************/
-      document.getElementById("reset").addEventListener("click", () => {
-        localStorage.clear();
-        scoreCount = 0;
-        clickPower = 1;
-        chargeLevelNumber = 1;
-        chargePriceAmount = 50;
-        etincelleLevelNumber = 1;
-        etincellePriceAmount = 200;
-        fatalfoudreLevelNumber = 1;
-        fatalfoudrePriceAmount = 500;
-        trempetteLevelNumber = 1;
-        trempettePriceAmount = 1000;
-        potionLevelNumber = 0;
-        potionPriceAmount = 500;
-        multiexpLevelNumber = 0;
-        multiexpPriceAmount = 2500;
-        terrainLevelNumber = 0;
-        terrainPriceAmount = 10000;
-        refreshCookieScore();
-        refreshEtincelleLevel();
-        refreshChargeLevel();
-        refreshFatalfoudreLevel();
-        refreshtrempetteLevel();
-        clearInterval(potionInt);
-        clearInterval(multiexpInt);
-        clearInterval(terrainInt);
-        refreshPotion();
-        refreshMultiexp();
-        refreshTerrain();
-        count = 0;
-        raichu.classList.add("hidden");
-        pikachu.classList.add("hidden");
-        pichu.classList.remove("hidden");
-        listpierre2.classList.add("hidden");
-        listpierre1.classList.remove("hidden");
-        allMultiple=0;
-      })
+  document.getElementById("reset").addEventListener("click", () => {
+    scoreCount = 0;
+    clickPower = 1;
+    refreshCookieScore();
+    items.forEach(item => {
+        clearInterval(itemInt);
+        item.level = 1;
+        item.price = item.initialPrice;
+        refreshItemLevel(item);
+    });
+
+    itemsData.forEach(item => {
+        clearInterval(item.interval);
+        item.level = 1;
+        item.price = item.initialPrice;
+        refreshItem(item);
+    });
+    
+  });
+          
 });
