19b6a051d227562e04df95d0f061f9d19331d5f8
Eneuem <nemozor@gmail.com>
2023-10-27 10:47:57 +0200

    modif

--- script.js
+++ script.js
@@ -42,19 +42,19 @@ refreshCookieScore();
                   Game-Over
   
   ********************************/
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("myBtn");
-var span = document.getElementsByClassName("close")[0];
-span.addEventListener("click", reset);
+// var modal = document.getElementById("myModal");
+// var btn = document.getElementById("myBtn");
+// var span = document.getElementsByClassName("close")[0];
+// span.addEventListener("click", reset);
 
-btn.addEventListener("click", GameOver);
-function GameOver(){
-modal.style.display="block";
-reset();
-span.onclick = function() {
-  modal.style.display = "none";
-}
-}
+// btn.addEventListener("click", GameOver);
+// function GameOver(){
+// modal.style.display="block";
+// reset();
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+// }
   
     /********************************
   
